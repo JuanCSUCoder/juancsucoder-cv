8fa65acf69982df49892227895d60ee4a0c26406
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-12-13 14:34:28 -0500

    Feat: Improve Styling

--- app/ui/stack-ui/TechCard.tsx
+++ app/ui/stack-ui/TechCard.tsx
@@ -7,7 +7,7 @@ type TechType = {
 
 export default function TechCard({ tech }: { tech: TechType }) {
   return (
-		<div className="m-1 p-3 border-black border-solid border-2 rounded hover:bg-gray-200 flex flex-col justify-center items-center">
+		<div className="m-1 p-3 border-black border-solid border-2 rounded hover:bg-gray-200 flex flex-col justify-center items-center shadow-lg shadow-black">
 			<Image
 				src={`/images/${tech.logo}`}
 				alt={`Logo of ${tech.name} technology`}
