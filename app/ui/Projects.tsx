17e99a4eb8a3236e5940f7461474fdd6d02ce432
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-12-13 15:39:56 -0500

    Feat: Links and Images for Projects

--- app/ui/Projects.tsx
+++ app/ui/Projects.tsx
@@ -13,35 +13,46 @@ const projects: ProjectType[] = [
   {
     name: "Flatboat Project",
     link: "https://github.com/JuanCSUCoder/FlatBoatProject",
-    logo: "flatboat.png"
+    logo: "flatboat.png",
   },
   {
     name: "RobotEn",
     link: "https://github.com/JuanCSUCoder/RobotEn",
+    logo: "roboten.svg",
   },
   {
     name: "ViRust",
+    link: "https://github.com/JuanCSUCoder/ViRust",
   },
   {
     name: "CertAsset",
+    link: "https://github.com/JuanCSUCoder/CertAsset",
   },
   {
     name: "Caseta",
+    link: "https://caseta-web-app.vercel.app/",
+    logo: "caseta.webp",
   },
   {
     name: "Baxter Interface 2",
+    link: "https://github.com/JuanCSUCoder/baxter_interface_2",
   },
   {
     name: "Docker GNS3 Appliances",
+    link: "https://github.com/JuanCSUCoder/GNS3Appliances",
   },
   {
     name: "SystemicWorks",
+    link: "https://juancsucoder.github.io/SystemicWorks/",
+    logo: "systemic.svg",
   },
   {
     name: "TerMoney",
+    link: "https://github.com/JuanCSUCoder/TerMoney",
   },
   {
     name: "Hamachi with Haguichi in Docker",
+    link: "https://github.com/JuanCSUCoder/HamachiHaguichiDocker",
   },
 ];
 
