97d6774ad5fb39facad862d4d7149890e6c39f9d
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-12-13 15:05:11 -0500

    Feat: Listado de Proyectos

--- app/ui/Projects.tsx
+++ app/ui/Projects.tsx
@@ -2,7 +2,40 @@ import Subtitle from "@/components/basic/Subtitle";
 
 const projects = [
   {
-    name: "Flatboat"
+    name: "Flatboat CLI"
+  },
+  {
+    name: "Flatboat Images"
+  },
+  {
+    name: "Flatboat Templates"
+  },
+  {
+    name: "RobotEn"
+  },
+  {
+    name: "ViRust"
+  },
+  {
+    name: "CertAsset"
+  },
+  {
+    name: "Caseta"
+  },
+  {
+    name: "Baxter Interface 2"
+  },
+  {
+    name: "Docker GNS3 Appliances"
+  },
+  {
+    name: "SystemicWorks"
+  },
+  {
+    name: "TerMoney"
+  },
+  {
+    name: "Hamachi with Haguichi in Docker"
   }
 ]
 
