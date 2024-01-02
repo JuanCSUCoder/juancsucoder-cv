29f1ecf6e581f311fa79267360e9f4d044a8e885
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-01-02 10:49:33 -0500

    Feat: Adds Some Languages

--- app/ui/Langs.tsx
+++ app/ui/Langs.tsx
@@ -6,13 +6,41 @@ import TechCard, { TechType } from "../components/custom/TechCard"
 const languages: TechType[] = [
   {
     name: "JavaScript",
-    logo: "javascript.svg"
+    logo: "javascript.svg",
   },
   {
     name: "TypeScript",
-    logo: "typescript.svg"
-  }
-]
+    logo: "typescript.svg",
+  },
+  {
+    name: "Rust Lang",
+    logo: "rust.svg",
+  },
+  {
+    name: "Python",
+    logo: "python.svg",
+  },
+  {
+    name: "Java",
+    logo: "java.svg",
+  },
+  {
+    name: "Kotlin",
+    logo: "kotlin.svg",
+  },
+  {
+    name: "C++",
+    logo: "c++.svg",
+  },
+  {
+    name: "HTML 5",
+    logo: "html5.svg",
+  },
+  {
+    name: "CSS",
+    logo: "css.svg",
+  },
+];
 
 export default function Langs() {
   return (
